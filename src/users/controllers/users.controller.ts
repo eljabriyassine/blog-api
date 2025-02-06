0d471fd2aa2ee4f2d5736d147ec382b4e1a5df20
eljabriyassine <yassinejabrio.2001@gmail.com>
2025-02-06 12:17:00 +0100

    feat: add user module-service-controllers

---
+++ src/users/controllers/users.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from '../service/users.service';
+import { User, UserRole } from '../models/user.interface';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { get } from 'http';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/all')
+  findAllUsers(): Promise<User[]> {
+    return this.usersService.findAllUsers();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findUserById(id);
+  }
+
+  @Get('/search/:query')
+  findUserByEmailOrUsername(@Param('query') query: string): Promise<User> {
+    return this.usersService.findUserByEmailOrUsername(query);
+  }
+
+  @Post()
+  createUser(@Body() userDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Patch(':id/role')
+  changeUserRole(
+    @Param('id') id: string,
+    @Body('role') role: UserRole,
+  ): Promise<User> {
+    return this.usersService.changeUserRole(id, role);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string): Promise<{ message: string }> {
+    return this.usersService.deleteUser(id);
+  }
+}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { UserRole } from '../models/user.interface';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'The name of the user' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'The username of the user' })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @ApiProperty({ description: 'The email of the user' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ description: 'The password of the user' })
+  @IsString()
+  password: string;
+
+  @ApiProperty({ description: 'The role of the user' })
+  @IsEnum(UserRole)
+  role: UserRole;
+}
